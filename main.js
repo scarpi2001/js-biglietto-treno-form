d4ffdec8b7a7b7eca78ac4c0028222f22c1bc129
Marco <posta.email3001@gmail.com>
2022-11-08 15:21:01 +0100

    js-base_completo

--- main.js
+++ main.js
@@ -1,49 +1,46 @@
 //chiedi all'utente numero chilometri ed età, sulla base di questo calcola il prezzo del biglietto (€ 0,21 al km) applica 20% di sconto se minorenne 40% se over 65
 
-//variabili
+//variabili costanti
 const myButton = document.getElementById("create");
 const inputFieldName = document.getElementById("name");
-const inputFieldKm = document.getElementById("chilometri");
-const inputFieldAge = document.getElementById("eta");
-let nome;
-let chilometri;
-let eta;
-let prezzoBase;
-
-
+const inputFieldKm = document.getElementById("km");
+const inputFieldAge = document.getElementById("age");
 
 myButton.addEventListener("click",
 
     function(){
-        nome = inputFieldName.value;
-        chilometri = parseInt(inputFieldKm.value);
-        eta = inputFieldAge.value;
-        console.log(nome, chilometri, eta);
+        //chiedi dati e calcola prezzo base
+        let nome = inputFieldName.value;
+        let chilometri = parseInt(inputFieldKm.value);
+        let eta = inputFieldAge.value;
 
-        prezzoBase = chilometri * 0.21;
-        document.getElementById("prezzo_base").innerHTML = `Prezzo base ${prezzoBase.toFixed(2)}`
+        let prezzo = chilometri * 0.21;
+        document.getElementById("prezzo_base").innerHTML = `Prezzo base ${prezzo.toFixed(2)}`
+
+        console.log(nome, chilometri, eta, prezzo);
+        
+        //sconti
+        let sconto;
+
+        if (eta === "minorenne") {
+            sconto = prezzo * 0.20;
+            prezzo -= sconto;
+            console.log("sconto minore", sconto);
+            document.getElementById("prezzo_scontato").innerHTML = `Prezzo scontato €${prezzo.toFixed(2)} / Sconto minorenne €${sconto.toFixed(2)} `;
+        } else if (eta === "over65") {
+            sconto = prezzo * 0.40;
+            prezzo -= sconto;
+            console.log("sconto maggiore", sconto);
+            document.getElementById("prezzo_scontato").innerHTML = `Prezzo scontato €${prezzo.toFixed(2)} / Sconto over 65 €${sconto.toFixed(2)} `;
+        }
+
+        console.log("prezzo scontato", prezzo);
+        inputFieldName.value = "";
+        inputFieldKm.value = "";
+
     }
 
 );
 
 
-
-
-//sconti
-// let sconto;
-// let prezzoScontato;
-
-// if (eta === minorenne) {
-//     sconto = prezzoBase * 0.20;
-//     prezzoScontato = prezzoBase - sconto;
-//     console.log("sconto minore", sconto);
-//     document.getElementById("prezzo_scontato").innerHTML = `Prezzo scontato €${prezzoScontato.toFixed(2)} / Sconto minorenne €${sconto.toFixed(2)} `;
-// } else if (eta === over) {
-//     sconto = prezzoBase * 0.40;
-//     prezzoScontato = prezzoBase - sconto;
-//     console.log("sconto maggiore", sconto);
-//     document.getElementById("prezzo_scontato").innerHTML = `Prezzo scontato €${prezzoScontato.toFixed(2)} / Sconto over 65 €${sconto.toFixed(2)} `;
-// } else
-
-// console.log("prezzo scontato", prezzoScontato);
 
