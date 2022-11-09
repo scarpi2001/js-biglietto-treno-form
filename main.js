95a5c29929b8cc1a84491cbd82e6726a5d9431b4
Marco <posta.email3001@gmail.com>
2022-11-09 12:51:48 +0100

    fixed

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 //chiedi all'utente numero chilometri ed età, sulla base di questo calcola il prezzo del biglietto (€ 0,21 al km) applica 20% di sconto se minorenne 40% se over 65
 
-//variabili costanti
-const myButton = document.getElementById("create");
+//variabili e costanti
+const buttonCreate = document.getElementById("create");
+const buttonCancel = document.getElementById("cancel");
 const inputFieldName = document.getElementById("name");
 const inputFieldKm = document.getElementById("km");
 const inputFieldAge = document.getElementById("age");
+const ticket = document.querySelector(".ticket");
+const passengerName = document.getElementById("passenger_name");
+const carrozza = document.getElementById("carrozza"); 
+const cp = document.getElementById("cp");
+const price = document.getElementById("price");
+const discount = document.getElementById("discount");
 
-myButton.addEventListener("click",
+buttonCreate.addEventListener("click",
 
     function(){
         //chiedi dati e calcola prezzo base
@@ -15,33 +22,53 @@ myButton.addEventListener("click",
         let eta = inputFieldAge.value;
         let prezzo = chilometri * 0.21;
         
-        console.log(nome, chilometri, eta);
-        
+        //stampa nome
+        passengerName.innerHTML = `${nome}`;
+
         //sconti
         let sconto;
 
         if (eta === "minorenne") {
             sconto = prezzo * 0.20;
             prezzo -= sconto;
-            console.log("sconto minore", sconto);
-            document.getElementById("price").innerHTML = `Prezzo scontato €${prezzo.toFixed(2)} / Sconto minorenne €${sconto.toFixed(2)} `;
+            //stampa
+            price.innerHTML = `€${prezzo.toFixed(2)}`;
+            discount.innerHTML = `Biglietto minorenne`;
         } else if (eta === "over65") {
             sconto = prezzo * 0.40;
             prezzo -= sconto;
-            console.log("sconto maggiore", sconto);
-            document.getElementById("price").innerHTML = `Prezzo scontato €${prezzo.toFixed(2)} / Sconto over 65 €${sconto.toFixed(2)} `;
+            //stampa
+            price.innerHTML = `€${prezzo.toFixed(2)}`;
+            discount.innerHTML = `Biglietto over 65`;
         } else {
-            document.getElementById("price").innerHTML = `Prezzo base ${prezzo.toFixed(2)}`;
+            //stampa
+            price.innerHTML = `€${prezzo.toFixed(2)}`;
+            discount.innerHTML = `Biglietto standard`;
         }
-            
+    
+        //stampa codice cp e carrozza (numeri casuali)
+        carrozza.innerHTML = Math.floor(Math.random() * 11);
+        cp.innerHTML = Math.floor(Math.random() * 100000);
 
-        console.log("prezzo", prezzo);
-        inputFieldName.value = "";
-        inputFieldKm.value = "";
-
+        //apparizione biglietto
+        ticket.className = "active";
     }
 
 );
+
+buttonCancel.addEventListener("click",
+
+    function(){
+        //scomparsa biglietto
+        ticket.className = "deactive";
+
+        //reset input fields
+        inputFieldName.value = "";
+        inputFieldKm.value = "";
+    }
+
+);
+
 
 
 
